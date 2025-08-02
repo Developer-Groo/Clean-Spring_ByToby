9ffd47f77b4c3e3baca724f81795d01fc37eb5b0
Groo <woohyeon3382@naver.com>
2025-08-02 20:19:18 +0900

    feat: MemberTest 작성

--- splearn/src/test/java/tobyspring/splearn/domain/MemberTest.java
+++ splearn/src/test/java/tobyspring/splearn/domain/MemberTest.java
@@ -15,4 +15,61 @@ class MemberTest {
         assertThat(member.getStatus()).isEqualTo(MemberStatus.PENDING);
     }
 
+    @Test
+    void constructorNullCheck() {
+        // when & then
+        assertThatThrownBy(() -> new Member(null, "nickname", "password"))
+                .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    void activate() {
+        // given
+        var member = new Member("email", "nickname", "password");
+
+        // when
+        member.activate();
+
+        // then
+        assertThat(member.getStatus()).isEqualTo(MemberStatus.ACTIVE);
+    }
+
+    @Test
+    void activateFail() {
+        // given
+        var member = new Member("email", "nickname", "password");
+
+        // when
+        member.activate();
+
+        // then
+        assertThatThrownBy(member::activate).isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void deactivate() {
+        // given
+        var member = new Member("email", "nickname", "password");
+        member.activate();
+
+        // when
+        member.deactivate();
+
+        // then
+        assertThat(member.getStatus()).isEqualTo(MemberStatus.DEACTIVATED);
+    }
+
+    @Test
+    void deactivateFail() {
+        // given
+        var member = new Member("email", "nickname", "password");
+
+        // when & then
+        assertThatThrownBy(member::deactivate).isInstanceOf(IllegalStateException.class);
+
+        member.activate();
+        member.deactivate();
+
+        assertThatThrownBy(member::deactivate).isInstanceOf(IllegalStateException.class);
+    }
 }
