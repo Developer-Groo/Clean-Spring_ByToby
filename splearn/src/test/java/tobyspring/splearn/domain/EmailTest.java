dc7002e72f53eacd741fdea7d7e05b709154dd7a
Groo <woohyeon3382@naver.com>
2025-09-11 23:04:09 +0900

    chore: import 정리

--- splearn/src/test/java/tobyspring/splearn/domain/EmailTest.java
+++ splearn/src/test/java/tobyspring/splearn/domain/EmailTest.java
@@ -2,7 +2,7 @@ package tobyspring.splearn.domain;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class EmailTest {
 
