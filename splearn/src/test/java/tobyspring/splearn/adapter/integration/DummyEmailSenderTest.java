0fec8e41b57d8d9baef71c4dcf6c795bef993f5e
Groo <woohyeon3382@naver.com>
2025-09-11 23:02:32 +0900

    feat: EmailSenderTest 작성

---
+++ splearn/src/test/java/tobyspring/splearn/adapter/integration/DummyEmailSenderTest.java
@@ -0,0 +1,22 @@
+package tobyspring.splearn.adapter.integration;
+
+import org.junit.jupiter.api.Test;
+import org.junitpioneer.jupiter.StdIo;
+import org.junitpioneer.jupiter.StdOut;
+import tobyspring.splearn.domain.Email;
+
+import static org.assertj.core.api.Assertions.*;
+
+class DummyEmailSenderTest {
+
+    @Test
+    @StdIo
+    void dummyEmailSend(StdOut out) {
+        DummyEmailSender dummyEmailSender = new DummyEmailSender();
+
+        dummyEmailSender.send(new Email("splearn@gmail.com"), "subject", "body");
+
+        assertThat(out.capturedLines()[0])
+                .isEqualTo("DummyEmailSender send email: Email[address=splearn@gmail.com]");
+    }
+}
