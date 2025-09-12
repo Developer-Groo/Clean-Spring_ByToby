481515d091ee39740127bd00f1a6183ce941ecb8
Groo <woohyeon3382@naver.com>
2025-09-12 18:41:47 +0900

    feat: SecurePasswordEncoder 테스트 작성

---
+++ splearn/src/test/java/tobyspring/splearn/adapter/security/SecurePasswordEncoderTest.java
@@ -0,0 +1,21 @@
+package tobyspring.splearn.adapter.security;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class SecurePasswordEncoderTest {
+
+    @Test
+    void securePasswordEncoder() {
+        // given
+        SecurePasswordEncoder securePasswordEncoder = new SecurePasswordEncoder();
+
+        // when
+        String passwordHash = securePasswordEncoder.encode("password");
+
+        // then
+        assertThat(securePasswordEncoder.matches("password", passwordHash)).isTrue();
+        assertThat(securePasswordEncoder.matches("secret", passwordHash)).isFalse();
+    }
+}
