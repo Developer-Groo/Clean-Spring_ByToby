335ee7d2b6648729f53d466b7f204cc77e6ddba2
Groo <woohyeon3382@naver.com>
2025-09-15 16:10:07 +0900

    chore: 사용하지 않는 import 제거

--- splearn/src/test/java/tobyspring/splearn/domain/member/MemberTest.java
+++ splearn/src/test/java/tobyspring/splearn/domain/member/MemberTest.java
@@ -1,7 +1,6 @@
 package tobyspring.splearn.domain.member;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
