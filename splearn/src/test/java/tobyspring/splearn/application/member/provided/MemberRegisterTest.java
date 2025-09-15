ea9d95aa0dafa8413a8e4d7e389585626ae73785
Groo <woohyeon3382@naver.com>
2025-09-15 18:09:10 +0900

    test: updateInfo 실패 케이스 작성

--- splearn/src/test/java/tobyspring/splearn/application/member/provided/MemberRegisterTest.java
+++ splearn/src/test/java/tobyspring/splearn/application/member/provided/MemberRegisterTest.java
@@ -79,6 +79,37 @@ record MemberRegisterTest(MemberRegister memberRegister, EntityManager entityMan
     }
 
     @Test
+    void updateInfoFail() {
+        Member member = registerMember();
+        memberRegister.activate(member.getId());
+        memberRegister.updateInfo(member.getId(), new MemberInfoUpdateRequest("Toby123", "toby100", "자기소개"));
+
+        Member member2 = registerMember("splearn2@gmail.com");
+        memberRegister.activate(member2.getId());
+        entityManager.flush();
+        entityManager.clear();
+
+        // member2 는 기존의 member 와 같은 프로필 주소를 사용할 수 없다
+        assertThatThrownBy(() ->
+                memberRegister.updateInfo(member2.getId(), new MemberInfoUpdateRequest("Peter", "toby100", "Introduction"))
+        ).isInstanceOf(DuplicateProfileException.class);
+
+        // 다른 프로필 주소로는 변경 가능
+        memberRegister.updateInfo(member2.getId(), new MemberInfoUpdateRequest("Peter", "toby101", "Introduction"));
+
+        // 기존 프로필 주소 변경 가능
+        memberRegister.updateInfo(member.getId(), new MemberInfoUpdateRequest("Peter", "toby100", "Introduction"));
+
+        // 프로필 주소 제거 가능
+        memberRegister.updateInfo(member.getId(), new MemberInfoUpdateRequest("Peter", "", "Introduction"));
+
+        // 프로필 주소 중복은 허용하지 않음
+        assertThatThrownBy(() ->
+                memberRegister.updateInfo(member.getId(), new MemberInfoUpdateRequest("Peter", "toby101", "Introduction"))
+        ).isInstanceOf(DuplicateProfileException.class);
+    }
+
+    @Test
     void memberRegisterRequestFail() {
         // when & then
         checkValidation(new MemberRegisterRequest("splearn@gmail.com", "name", "password"));
@@ -88,6 +119,13 @@ record MemberRegisterTest(MemberRegister memberRegister, EntityManager entityMan
 
     private Member registerMember() {
         Member member = memberRegister.register(MemberFixture.createMemberRegisterRequest());
+        entityManager.flush();
+        entityManager.clear();
+        return member;
+    }
+
+    private Member registerMember(String email) {
+        Member member = memberRegister.register(MemberFixture.createMemberRegisterRequest(email));
         entityManager.flush();
         entityManager.clear();
         return member;
